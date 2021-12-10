6ee71539a41abd64a6d4f8d7d0d51f7049ea4268
marlenakuc95 <marlena.kuc95@gmail.com>
2021-12-10 13:55:06 +0100

    Slurm scheduling

---
+++ slurm/base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks=2
+#SBATCH --job-name=distill
+#SBATCH --gres=gpu:2
+#SBATCH -o /pfs/work7/workspace/scratch/ma_mkuc-clkd_data/slurm/outputs/slurm-%j.out
+#SBATCH --mail-user=mkuc@mail.uni-mannheim.de
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+now=$(date +"%T")
+echo "Program starts:  $now"
+
+# Activate conda env
+srun $1
+# Run script
+srun python /home/ma/ma_ma/$SLURM_JOB_USER/clkd/run.py
+
+end=$(date +"%T")
+echo "Completed: $end"
